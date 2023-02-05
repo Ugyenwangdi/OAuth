bb4ed58de68ed7858844fccdad93c4a508b9e66c
Wulfi <57824653+Ugyenwangdi@users.noreply.github.com>
2023-02-05 14:00:58 +0600

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
 
 
 
